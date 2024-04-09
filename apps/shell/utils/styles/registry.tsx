97dab02b2d8ad9cb2854a7335ea9bd1fca22f38b
Abhiraj Kumar <abhirajk@adobe.com>
2024-04-09 21:46:32 +0530

    fix: app router build

--- apps/shell/utils/styles/registry.tsx
+++ apps/shell/utils/styles/registry.tsx
@@ -3,6 +3,10 @@
 import React, { useState } from 'react';
 import { useServerInsertedHTML } from 'next/navigation';
 import { StyleRegistry, createStyleRegistry } from 'styled-jsx';
+import {
+  Provider as SpectrumProvider,
+  lightTheme,
+} from '@adobe/react-spectrum';
 
 export function StyledJsxRegistry({ children }: { children: React.ReactNode }) {
   // Only create stylesheet once with lazy initial state
@@ -15,5 +19,9 @@ export function StyledJsxRegistry({ children }: { children: React.ReactNode }) {
     return <>{styles}</>;
   });
 
-  return <StyleRegistry registry={jsxStyleRegistry}>{children}</StyleRegistry>;
+  return <StyleRegistry registry={jsxStyleRegistry}>
+      <SpectrumProvider theme={lightTheme} colorScheme="light">
+        {children}
+      </SpectrumProvider>
+    </StyleRegistry>;
 }
